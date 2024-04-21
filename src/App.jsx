9130a87f7485d1bf3c400d9e80f1026b56f911a9
Ziad Hussein <zh1313030@gmail.com>
2024-04-21 03:48:19 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TicTacToe from './Components/TicTacToe/TicTacToe.jsx'
+
+
+const App = () => {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  )
+}
+
+export default App
